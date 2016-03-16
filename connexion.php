7f6668077de18c82022896797247f74dad9a7928
Xeiis <seis02@hotmail.fr>
2016-03-16 14:24:50 +0100

    euhh ca marche ??

--- connexion.php
+++ connexion.php
@@ -8,8 +8,8 @@ catch (PDOException $e)
     die();
 }
 
-$login = $_POST['login'];
-$password = $_POST['password'];
+$login = (isset($_POST["login"])) ? htmlentities($_POST["login"]) : NULL;
+$password = (isset($_POST["password"])) ? htmlentities($_POST["password"]) : NULL;
 
 $sql = "select 1 from UTILISATEUR where identifiant = '".$login."' and password = '".$password;
 $reponse = $dbh->query($sql);
